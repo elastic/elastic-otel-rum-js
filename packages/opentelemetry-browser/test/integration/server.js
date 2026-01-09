82c7365a52ea47f0ed8dd94b35da5e37c9f2eb99
David Luna <luna.bistuer@gmail.com>
2026-01-09 17:31:35 +0100

    chore: inject EDOT in integration fixtures

--- packages/opentelemetry-browser/test/integration/server.js
+++ packages/opentelemetry-browser/test/integration/server.js
@@ -8,16 +8,15 @@
 //  -> @opentelemetry/otlp-transformer
 //      -> protobufjs
 //          -> @types/node
-// it's okay for now but wee need to explicitly install once this
-// transitive deps are 
+// it's okay for now but wee need to add a 3-slash reference once this
+// transitive deps are resolved
 
-import { createReadStream, existsSync, lstatSync, readFileSync } from 'fs';
+import { createReadStream, lstatSync, readFileSync } from 'fs';
 import { createServer } from 'http';
 
 const server = createServer((req, res) => {
     // sanitize the path
     const url = new URL(`http://localhost${req.url}`);
-    const config = url.searchParams.get('config') || 'null';
 
     // this is for the ping that playwright does before testing
     if (url.pathname === '/') {
@@ -30,21 +29,23 @@ const server = createServer((req, res) => {
     const lstat = lstatSync(fileUrl, {throwIfNoEntry: false});
 
     if (lstat && lstat.isFile()) {
-        console.log('file found')
         // Assets are meant to be only HTML pages but we could allow others
         if (fileUrl.pathname.endsWith('.html')) {
             const origHtml = readFileSync(fileUrl, { encoding: 'utf-8' });
+            const config = url.searchParams.get('config') || 'null';
 
-            // TODO: inject the EDOT with config
-            res.end(origHtml.replace(
-                'globalThis.edotConfig = null;',
-                `globalThis.edotConfig = JSON.parse(decodeURIComponent('${config}'))`,
-            ));
+            // inject the EDOT with config
+            res.end(injectSdk(origHtml, JSON.parse(decodeURIComponent(config))))
             return;
         }
 
         // For the rest just pipe
         const fileStream = createReadStream(fileUrl);
+        if (fileUrl.pathname.endsWith('.js')) {
+            res.setHeader('content-type', 'application/javascript');
+        } else if (fileUrl.pathname.endsWith('.css')) {
+            res.setHeader('content-type', 'text/css');
+        }
         fileStream.pipe(res);
     } else {
         res.writeHead(404, 'Not Found');
@@ -54,3 +55,22 @@ const server = createServer((req, res) => {
 
 server.listen(3000);
 console.log(`server listening to http://localhost:${server.address().port}`)
+
+// -- helper functions
+
+function injectSdk(html, config) {
+    const code = `
+        <script>
+        // Same pattern as https://www.elastic.co/docs/reference/apm/agents/rum-js/install-agent#_asynchronous_non_blocking_pattern
+        ;(function(d, s, c) {
+            var j = d.createElement(s),
+                t = d.getElementsByTagName(s)[0];
+            j.src = './assets/elastic-otel-browser.min.js';
+            j.onload = function() { startBrowserSdk(c); };
+            t.parentNode.insertBefore(j, t);
+        })(document, 'script', ${config})
+        </script>
+    `;
+
+    return html.replace('<!-- EDOT_PLACEHOLDER -->', code);
+}
