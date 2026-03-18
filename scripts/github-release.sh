10ee7c7729929e5ba553d57dc412272452c0ce9a
David Luna <luna.bistuer@gmail.com>
2026-03-18 10:33:22 +0100

    chore: add missing license headers

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copyright Elasticsearch B.V. and contributors
+# SPDX-License-Identifier: Apache-2.0
+
 # Create a GitHub release.
 # (This is typically only run from the release.yml CI workflow.)
 #
