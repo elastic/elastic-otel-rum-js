a7b0719a5dee457d3087ece358b1ad0f184fca7d
David Luna <luna.bistuer@gmail.com>
2026-03-18 18:11:56 +0100

    chore: remove comments

--- packages/opentelemetry-browser/test/bundle/webpack4/src/index.js
+++ packages/opentelemetry-browser/test/bundle/webpack4/src/index.js
@@ -4,20 +4,6 @@
  */
 
 import {startBrowserSdk} from '@elastic/opentelemetry-browser';
-// import { logs } from '@opentelemetry/api-logs';
 
-// Setup SDK and register with API, then obtain logger from API.
+// Start the SDK with the default options
 startBrowserSdk();
-// const logger = logs.getLogger('bundle-test-webpack');
-
-// window.addEventListener('load', () => {
-//   console.log('loaded');
-//   const emitEventButton = document.getElementById('emit-event-button');
-//   emitEventButton.addEventListener('click', () => {
-//     console.log('clicked');
-//     logger.emit({
-//       body: 'test-event-body',
-//       eventName: 'custom.event',
-//     });
-//   });
-// });
