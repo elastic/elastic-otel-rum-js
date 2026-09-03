1a5e5154bc4c666bf434bdac2b0a2551fe2850ae
David Luna <luna.bistuer@gmail.com>
2026-09-03 08:00:21 +0200

    chore: update types

--- packages/opentelemetry-browser/types/context.d.ts
+++ packages/opentelemetry-browser/types/context.d.ts
@@ -1,4 +1,3 @@
 /** @type {import('@opentelemetry/api').ContextManager} */
 export const AsyncApisContextManager: import("@opentelemetry/api").ContextManager;
-export type AnyFunction = (...args: any[]) => any;
 export type FunctionKeys<T> = { [K in keyof T]: T[K] extends ((...args: any[]) => any) | undefined ? K : never; }[keyof T];
